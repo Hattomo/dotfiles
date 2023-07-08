5d00a5f46dbe57b8a68bcaf296b7faa620ef10af
Hattomo <45988993+Hattomo@users.noreply.github.com>
2023-07-08 23:01:14 +0900

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,6 @@
 # variable setup
 dotfiles_dir=~/dotfiles
 
-if "$CODESPACES" == "true"; then
-    rm .bash_logout
-else
-    cat ssh-agent.sh >> .bashrc
-    sh git-setup.sh
-fi
 
 # remove files
 sudo rm -rf ~/.bashrc > /dev/null 2>&1
@@ -15,9 +9,17 @@ sudo rm -rf ~/.gitconfig > /dev/null 2>&1
 sudo rm -rf ~/.bash_profile > /dev/null 2>&1
 sudo rm -rf ~/.bash_logout > /dev/null 2>&1
 
+
 # set symlinks
 ln -sf $dotfiles_dir/.tmux.conf ~/.tmux.conf
 ln -sf $dotfiles_dir/.bashrc ~/.bashrc
 ln -sf $dotfiles_dir/.bash_profile ~/.bash_profile
 ln -sf $dotfiles_dir/.bash_logout ~/.bash_logout
 ln -sf $dotfiles_dir/.gitconfig ~/.gitconfig
+
+if "$CODESPACES" == "true"; then
+    echo "Codespaces detected"
+else
+    cat ssh-agent.sh >> .bashrc
+    sh git-setup.sh
+fi
