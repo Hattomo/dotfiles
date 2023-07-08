4035bce8f8a47be01b65850af2c5423a7e3f8aca
Hattomo <45988993+Hattomo@users.noreply.github.com>
2023-07-08 22:02:18 +0900

    Set symlinks

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,16 @@
+# variable setup
+dotfiles_dir=~/dotfiles
+
 if "$CODESPACES" == "true"; then
     rm .bash_logout
 else
     cat ssh-agent.sh >> .bashrc
     sh git-setup.sh
 fi
+
+# set symlinks
+ln -sf $dotfiles_dir/.tmux.conf ~/.tmux.conf
+ln -sf $dotfiles_dir/.bashrc ~/.bashrc
+ln -sf $dotfiles_dir/.bash_profile ~/.bash_profile
+ln -sf $dotfiles_dir/.bash_logout ~/.bash_logout
+ln -sf $dotfiles_dir/.gitconfig ~/.gitconfig
