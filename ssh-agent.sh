64a574f022cf89adb9b63ae68456e0cb0f0c0933
Hattomo <45988993+Hattomo@users.noreply.github.com>
2023-07-08 22:38:45 +0900

    update

--- ssh-agent.sh
+++ ssh-agent.sh
@@ -1,4 +1,4 @@
 
 # for ssh-agent setting
-eval `ssh-agent`
-ssh-add $HOME/.ssh/hattomo-github-sign # stay fist for commit signing to use
+# eval `ssh-agent`
+# ssh-add $HOME/.ssh/hattomo-github-sign # stay fist for commit signing to use
