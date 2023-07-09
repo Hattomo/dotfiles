99aa133eb67ad0609384c0690195cc5d838645f1
Hattomo <45988993+Hattomo@users.noreply.github.com>
2023-07-09 18:34:14 +0900

    Update git-setup.sh

--- git-setup.sh
+++ git-setup.sh
@@ -1,2 +1,2 @@
-git config user.signingkey "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINUsl6iDJffCDZS3VWWZ3DhmCNxRsa4kg1H+beooKOgI hattomo-GitHub-main"
-git config gpg.format ssh
+git config --global user.signingkey "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINUsl6iDJffCDZS3VWWZ3DhmCNxRsa4kg1H+beooKOgI hattomo-GitHub-main"
+git config --global gpg.format ssh
